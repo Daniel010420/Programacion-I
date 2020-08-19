878a575e42b727e9ea5385980499c8a1790acccb
alexwam032120 <alexwam03@outlook.com>
2020-08-19 13:13:46 -0600

    Creacion de generador de tablas de multiplicar

--- Programacion-1/Programacion-1/Form1.Designer.vb
+++ Programacion-1/Programacion-1/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,202 +20,68 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblRespuesta = New System.Windows.Forms.Label()
-        Me.optResiduo = New System.Windows.Forms.RadioButton()
-        Me.optPorcentaje = New System.Windows.Forms.RadioButton()
-        Me.optExponenciacion = New System.Windows.Forms.RadioButton()
-        Me.optDivicion = New System.Windows.Forms.RadioButton()
-        Me.optMultiplicacion = New System.Windows.Forms.RadioButton()
-        Me.optResta = New System.Windows.Forms.RadioButton()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtnum2 = New System.Windows.Forms.TextBox()
+        Me.lsttabla = New System.Windows.Forms.ListBox()
+        Me.txttabla = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.optSuma = New System.Windows.Forms.RadioButton()
-        Me.txtnum1 = New System.Windows.Forms.TextBox()
-        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnGenerar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'lblRespuesta
+        'lsttabla
         '
-        Me.lblRespuesta.AutoSize = True
-        Me.lblRespuesta.Location = New System.Drawing.Point(298, 45)
-        Me.lblRespuesta.Name = "lblRespuesta"
-        Me.lblRespuesta.Size = New System.Drawing.Size(0, 13)
-        Me.lblRespuesta.TabIndex = 27
+        Me.lsttabla.FormattingEnabled = True
+        Me.lsttabla.Location = New System.Drawing.Point(153, 26)
+        Me.lsttabla.Name = "lsttabla"
+        Me.lsttabla.Size = New System.Drawing.Size(161, 186)
+        Me.lsttabla.TabIndex = 7
         '
-        'optResiduo
+        'txttabla
         '
-        Me.optResiduo.AutoSize = True
-        Me.optResiduo.Location = New System.Drawing.Point(143, 175)
-        Me.optResiduo.Name = "optResiduo"
-        Me.optResiduo.Size = New System.Drawing.Size(64, 17)
-        Me.optResiduo.TabIndex = 26
-        Me.optResiduo.TabStop = True
-        Me.optResiduo.Text = "Residuo"
-        Me.optResiduo.UseVisualStyleBackColor = True
-        '
-        'optPorcentaje
-        '
-        Me.optPorcentaje.AutoSize = True
-        Me.optPorcentaje.Location = New System.Drawing.Point(143, 129)
-        Me.optPorcentaje.Name = "optPorcentaje"
-        Me.optPorcentaje.Size = New System.Drawing.Size(76, 17)
-        Me.optPorcentaje.TabIndex = 25
-        Me.optPorcentaje.TabStop = True
-        Me.optPorcentaje.Text = "Porcentaje"
-        Me.optPorcentaje.UseVisualStyleBackColor = True
-        '
-        'optExponenciacion
-        '
-        Me.optExponenciacion.AutoSize = True
-        Me.optExponenciacion.Location = New System.Drawing.Point(143, 152)
-        Me.optExponenciacion.Name = "optExponenciacion"
-        Me.optExponenciacion.Size = New System.Drawing.Size(31, 17)
-        Me.optExponenciacion.TabIndex = 24
-        Me.optExponenciacion.TabStop = True
-        Me.optExponenciacion.Text = "^"
-        Me.optExponenciacion.UseVisualStyleBackColor = True
-        '
-        'optDivicion
-        '
-        Me.optDivicion.AutoSize = True
-        Me.optDivicion.Location = New System.Drawing.Point(143, 106)
-        Me.optDivicion.Name = "optDivicion"
-        Me.optDivicion.Size = New System.Drawing.Size(30, 17)
-        Me.optDivicion.TabIndex = 23
-        Me.optDivicion.TabStop = True
-        Me.optDivicion.Text = "/"
-        Me.optDivicion.UseVisualStyleBackColor = True
-        '
-        'optMultiplicacion
-        '
-        Me.optMultiplicacion.AutoSize = True
-        Me.optMultiplicacion.Location = New System.Drawing.Point(143, 83)
-        Me.optMultiplicacion.Name = "optMultiplicacion"
-        Me.optMultiplicacion.Size = New System.Drawing.Size(29, 17)
-        Me.optMultiplicacion.TabIndex = 22
-        Me.optMultiplicacion.TabStop = True
-        Me.optMultiplicacion.Text = "*"
-        Me.optMultiplicacion.UseVisualStyleBackColor = True
-        '
-        'optResta
-        '
-        Me.optResta.AutoSize = True
-        Me.optResta.Location = New System.Drawing.Point(143, 60)
-        Me.optResta.Name = "optResta"
-        Me.optResta.Size = New System.Drawing.Size(28, 17)
-        Me.optResta.TabIndex = 21
-        Me.optResta.TabStop = True
-        Me.optResta.Text = "-"
-        Me.optResta.UseVisualStyleBackColor = True
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(180, 41)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
-        Me.Label2.TabIndex = 20
-        Me.Label2.Text = "Numero 1"
-        '
-        'txtnum2
-        '
-        Me.txtnum2.Location = New System.Drawing.Point(239, 38)
-        Me.txtnum2.Name = "txtnum2"
-        Me.txtnum2.Size = New System.Drawing.Size(53, 20)
-        Me.txtnum2.TabIndex = 19
+        Me.txttabla.Location = New System.Drawing.Point(52, 23)
+        Me.txttabla.Name = "txttabla"
+        Me.txttabla.Size = New System.Drawing.Size(62, 20)
+        Me.txttabla.TabIndex = 6
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 41)
+        Me.Label1.Location = New System.Drawing.Point(9, 26)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 13)
-        Me.Label1.TabIndex = 18
-        Me.Label1.Text = "Numero 1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Tabla:"
         '
-        'optSuma
+        'btnGenerar
         '
-        Me.optSuma.AutoSize = True
-        Me.optSuma.Checked = True
-        Me.optSuma.Location = New System.Drawing.Point(143, 37)
-        Me.optSuma.Name = "optSuma"
-        Me.optSuma.Size = New System.Drawing.Size(31, 17)
-        Me.optSuma.TabIndex = 17
-        Me.optSuma.TabStop = True
-        Me.optSuma.Text = "+"
-        Me.optSuma.UseVisualStyleBackColor = True
-        '
-        'txtnum1
-        '
-        Me.txtnum1.Location = New System.Drawing.Point(72, 38)
-        Me.txtnum1.Name = "txtnum1"
-        Me.txtnum1.Size = New System.Drawing.Size(53, 20)
-        Me.txtnum1.TabIndex = 16
-        '
-        'cboOperaciones
-        '
-        Me.cboOperaciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cboOperaciones.FormattingEnabled = True
-        Me.cboOperaciones.Items.AddRange(New Object() {"Seleccione una opción", "Suma", "Resta", "Multiplicación", "División", "Porcentaje", "Exponenciación", "Residuo"})
-        Me.cboOperaciones.Location = New System.Drawing.Point(256, 125)
-        Me.cboOperaciones.Name = "cboOperaciones"
-        Me.cboOperaciones.Size = New System.Drawing.Size(121, 21)
-        Me.cboOperaciones.TabIndex = 15
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(183, 214)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(83, 53)
-        Me.Button1.TabIndex = 14
-        Me.Button1.Text = "Calcular"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnGenerar.Location = New System.Drawing.Point(12, 49)
+        Me.btnGenerar.Name = "btnGenerar"
+        Me.btnGenerar.Size = New System.Drawing.Size(115, 80)
+        Me.btnGenerar.TabIndex = 4
+        Me.btnGenerar.Text = "Generar"
+        Me.btnGenerar.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(396, 344)
-        Me.Controls.Add(Me.lblRespuesta)
-        Me.Controls.Add(Me.optResiduo)
-        Me.Controls.Add(Me.optPorcentaje)
-        Me.Controls.Add(Me.optExponenciacion)
-        Me.Controls.Add(Me.optDivicion)
-        Me.Controls.Add(Me.optMultiplicacion)
-        Me.Controls.Add(Me.optResta)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtnum2)
+        Me.ClientSize = New System.Drawing.Size(337, 253)
+        Me.Controls.Add(Me.lsttabla)
+        Me.Controls.Add(Me.txttabla)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.optSuma)
-        Me.Controls.Add(Me.txtnum1)
-        Me.Controls.Add(Me.cboOperaciones)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnGenerar)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Calculadora Basica"
+        Me.Text = "Tablas de multiplicar"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblRespuesta As Label
-    Friend WithEvents optResiduo As RadioButton
-    Friend WithEvents optPorcentaje As RadioButton
-    Friend WithEvents optExponenciacion As RadioButton
-    Friend WithEvents optDivicion As RadioButton
-    Friend WithEvents optMultiplicacion As RadioButton
-    Friend WithEvents optResta As RadioButton
-    Friend WithEvents Label2 As Label
-    Friend WithEvents txtnum2 As TextBox
+    Friend WithEvents lsttabla As ListBox
+    Friend WithEvents txttabla As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents optSuma As RadioButton
-    Friend WithEvents txtnum1 As TextBox
-    Friend WithEvents cboOperaciones As ComboBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnGenerar As Button
 End Class
--- Programacion-1/Programacion-1/Form1.vb
+++ Programacion-1/Programacion-1/Form1.vb
@@ -1,73 +1,17 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim num1, num2, respuesta As Double
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
 
-
-        num1 = txtnum1.Text
-        num2 = txtnum2.Text
-
+        Dim num, i As Int64
+        i = 1
+        num = txttabla.Text
 
 
-        If optSuma.Checked Then
-            respuesta = num1 + num2
-            lblRespuesta.Text = respuesta
-        End If
+        lsttabla.Items.Clear()
+        Do While i <= 10
+            lsttabla.Items.Add(num.ToString + "X" + i.ToString + "=" + (num * i).ToString)
+            i = i + 1
+        Loop
 
-        If optResta.Checked Then
-            respuesta = num1 - num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optMultiplicacion.Checked Then
-            respuesta = num1 * num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optDivicion.Checked Then
-            respuesta = num1 / num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optPorcentaje.Checked Then
-            respuesta = (num1 * num2) / 100
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optExponenciacion.Checked Then
-            respuesta = num1 ^ num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optResiduo.Checked Then
-            respuesta = num1 Mod num2
-            lblRespuesta.Text = respuesta
-        End If
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 1
-                respuesta = num1 + num2
-                lblRespuesta.Text = respuesta
-            Case 2
-                respuesta = num1 - num2
-                lblRespuesta.Text = respuesta
-            Case 3
-                respuesta = num1 * num2
-                lblRespuesta.Text = respuesta
-            Case 4
-                respuesta = num1 / num2
-                lblRespuesta.Text = respuesta
-            Case 5
-                respuesta = (num1 * num2) / 100
-                lblRespuesta.Text = respuesta
-            Case 6
-                respuesta = num1 ^ num2
-                lblRespuesta.Text = respuesta
-            Case 7
-                respuesta = num1 Mod num2
-                lblRespuesta.Text = respuesta
-        End Select
     End Sub
 End Class
