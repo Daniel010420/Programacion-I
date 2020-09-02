e3ade91817ecf1c0800d44d77494a8549f9b1474
alexwam032120 <alexwam03@outlook.com>
2020-09-02 11:31:48 -0600

    Combobox dinamico para departamento y municipio, datos no completados.

--- Programacion-1/Programacion-1/Form1.Designer.vb
+++ Programacion-1/Programacion-1/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,202 +20,69 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblRespuesta = New System.Windows.Forms.Label()
-        Me.optResiduo = New System.Windows.Forms.RadioButton()
-        Me.optPorcentaje = New System.Windows.Forms.RadioButton()
-        Me.optExponenciacion = New System.Windows.Forms.RadioButton()
-        Me.optDivicion = New System.Windows.Forms.RadioButton()
-        Me.optMultiplicacion = New System.Windows.Forms.RadioButton()
-        Me.optResta = New System.Windows.Forms.RadioButton()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtnum2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.optSuma = New System.Windows.Forms.RadioButton()
-        Me.txtnum1 = New System.Windows.Forms.TextBox()
-        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cobdepartamento = New System.Windows.Forms.ComboBox()
+        Me.cobmunicipio = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
-        '
-        'lblRespuesta
-        '
-        Me.lblRespuesta.AutoSize = True
-        Me.lblRespuesta.Location = New System.Drawing.Point(298, 45)
-        Me.lblRespuesta.Name = "lblRespuesta"
-        Me.lblRespuesta.Size = New System.Drawing.Size(0, 13)
-        Me.lblRespuesta.TabIndex = 27
-        '
-        'optResiduo
-        '
-        Me.optResiduo.AutoSize = True
-        Me.optResiduo.Location = New System.Drawing.Point(143, 175)
-        Me.optResiduo.Name = "optResiduo"
-        Me.optResiduo.Size = New System.Drawing.Size(64, 17)
-        Me.optResiduo.TabIndex = 26
-        Me.optResiduo.TabStop = True
-        Me.optResiduo.Text = "Residuo"
-        Me.optResiduo.UseVisualStyleBackColor = True
-        '
-        'optPorcentaje
-        '
-        Me.optPorcentaje.AutoSize = True
-        Me.optPorcentaje.Location = New System.Drawing.Point(143, 129)
-        Me.optPorcentaje.Name = "optPorcentaje"
-        Me.optPorcentaje.Size = New System.Drawing.Size(76, 17)
-        Me.optPorcentaje.TabIndex = 25
-        Me.optPorcentaje.TabStop = True
-        Me.optPorcentaje.Text = "Porcentaje"
-        Me.optPorcentaje.UseVisualStyleBackColor = True
-        '
-        'optExponenciacion
-        '
-        Me.optExponenciacion.AutoSize = True
-        Me.optExponenciacion.Location = New System.Drawing.Point(143, 152)
-        Me.optExponenciacion.Name = "optExponenciacion"
-        Me.optExponenciacion.Size = New System.Drawing.Size(31, 17)
-        Me.optExponenciacion.TabIndex = 24
-        Me.optExponenciacion.TabStop = True
-        Me.optExponenciacion.Text = "^"
-        Me.optExponenciacion.UseVisualStyleBackColor = True
-        '
-        'optDivicion
-        '
-        Me.optDivicion.AutoSize = True
-        Me.optDivicion.Location = New System.Drawing.Point(143, 106)
-        Me.optDivicion.Name = "optDivicion"
-        Me.optDivicion.Size = New System.Drawing.Size(30, 17)
-        Me.optDivicion.TabIndex = 23
-        Me.optDivicion.TabStop = True
-        Me.optDivicion.Text = "/"
-        Me.optDivicion.UseVisualStyleBackColor = True
-        '
-        'optMultiplicacion
-        '
-        Me.optMultiplicacion.AutoSize = True
-        Me.optMultiplicacion.Location = New System.Drawing.Point(143, 83)
-        Me.optMultiplicacion.Name = "optMultiplicacion"
-        Me.optMultiplicacion.Size = New System.Drawing.Size(29, 17)
-        Me.optMultiplicacion.TabIndex = 22
-        Me.optMultiplicacion.TabStop = True
-        Me.optMultiplicacion.Text = "*"
-        Me.optMultiplicacion.UseVisualStyleBackColor = True
-        '
-        'optResta
-        '
-        Me.optResta.AutoSize = True
-        Me.optResta.Location = New System.Drawing.Point(143, 60)
-        Me.optResta.Name = "optResta"
-        Me.optResta.Size = New System.Drawing.Size(28, 17)
-        Me.optResta.TabIndex = 21
-        Me.optResta.TabStop = True
-        Me.optResta.Text = "-"
-        Me.optResta.UseVisualStyleBackColor = True
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(180, 41)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
-        Me.Label2.TabIndex = 20
-        Me.Label2.Text = "Numero 1"
-        '
-        'txtnum2
-        '
-        Me.txtnum2.Location = New System.Drawing.Point(239, 38)
-        Me.txtnum2.Name = "txtnum2"
-        Me.txtnum2.Size = New System.Drawing.Size(53, 20)
-        Me.txtnum2.TabIndex = 19
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 41)
+        Me.Label1.Location = New System.Drawing.Point(12, 68)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 13)
-        Me.Label1.TabIndex = 18
-        Me.Label1.Text = "Numero 1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Depart."
         '
-        'optSuma
+        'Label2
         '
-        Me.optSuma.AutoSize = True
-        Me.optSuma.Checked = True
-        Me.optSuma.Location = New System.Drawing.Point(143, 37)
-        Me.optSuma.Name = "optSuma"
-        Me.optSuma.Size = New System.Drawing.Size(31, 17)
-        Me.optSuma.TabIndex = 17
-        Me.optSuma.TabStop = True
-        Me.optSuma.Text = "+"
-        Me.optSuma.UseVisualStyleBackColor = True
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(208, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Municip."
         '
-        'txtnum1
+        'cobdepartamento
         '
-        Me.txtnum1.Location = New System.Drawing.Point(72, 38)
-        Me.txtnum1.Name = "txtnum1"
-        Me.txtnum1.Size = New System.Drawing.Size(53, 20)
-        Me.txtnum1.TabIndex = 16
+        Me.cobdepartamento.FormattingEnabled = True
+        Me.cobdepartamento.Location = New System.Drawing.Point(66, 65)
+        Me.cobdepartamento.Name = "cobdepartamento"
+        Me.cobdepartamento.Size = New System.Drawing.Size(121, 21)
+        Me.cobdepartamento.TabIndex = 3
         '
-        'cboOperaciones
+        'cobmunicipio
         '
-        Me.cboOperaciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cboOperaciones.FormattingEnabled = True
-        Me.cboOperaciones.Items.AddRange(New Object() {"Seleccione una opción", "Suma", "Resta", "Multiplicación", "División", "Porcentaje", "Exponenciación", "Residuo"})
-        Me.cboOperaciones.Location = New System.Drawing.Point(256, 125)
-        Me.cboOperaciones.Name = "cboOperaciones"
-        Me.cboOperaciones.Size = New System.Drawing.Size(121, 21)
-        Me.cboOperaciones.TabIndex = 15
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(183, 214)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(83, 53)
-        Me.Button1.TabIndex = 14
-        Me.Button1.Text = "Calcular"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.cobmunicipio.FormattingEnabled = True
+        Me.cobmunicipio.Location = New System.Drawing.Point(261, 68)
+        Me.cobmunicipio.Name = "cobmunicipio"
+        Me.cobmunicipio.Size = New System.Drawing.Size(121, 21)
+        Me.cobmunicipio.TabIndex = 4
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(396, 344)
-        Me.Controls.Add(Me.lblRespuesta)
-        Me.Controls.Add(Me.optResiduo)
-        Me.Controls.Add(Me.optPorcentaje)
-        Me.Controls.Add(Me.optExponenciacion)
-        Me.Controls.Add(Me.optDivicion)
-        Me.Controls.Add(Me.optMultiplicacion)
-        Me.Controls.Add(Me.optResta)
+        Me.ClientSize = New System.Drawing.Size(412, 160)
+        Me.Controls.Add(Me.cobmunicipio)
+        Me.Controls.Add(Me.cobdepartamento)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtnum2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.optSuma)
-        Me.Controls.Add(Me.txtnum1)
-        Me.Controls.Add(Me.cboOperaciones)
-        Me.Controls.Add(Me.Button1)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Calculadora Basica"
+        Me.Text = "Combo dinamico"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblRespuesta As Label
-    Friend WithEvents optResiduo As RadioButton
-    Friend WithEvents optPorcentaje As RadioButton
-    Friend WithEvents optExponenciacion As RadioButton
-    Friend WithEvents optDivicion As RadioButton
-    Friend WithEvents optMultiplicacion As RadioButton
-    Friend WithEvents optResta As RadioButton
-    Friend WithEvents Label2 As Label
-    Friend WithEvents txtnum2 As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents optSuma As RadioButton
-    Friend WithEvents txtnum1 As TextBox
-    Friend WithEvents cboOperaciones As ComboBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cobdepartamento As ComboBox
+    Friend WithEvents cobmunicipio As ComboBox
 End Class
--- Programacion-1/Programacion-1/Form1.vb
+++ Programacion-1/Programacion-1/Form1.vb
@@ -1,73 +1,45 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim num1, num2, respuesta As Double
+    Dim objdireccion = New direcciones()
 
-
-        num1 = txtnum1.Text
-        num2 = txtnum2.Text
-
-
-
-        If optSuma.Checked Then
-            respuesta = num1 + num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optResta.Checked Then
-            respuesta = num1 - num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optMultiplicacion.Checked Then
-            respuesta = num1 * num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optDivicion.Checked Then
-            respuesta = num1 / num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optPorcentaje.Checked Then
-            respuesta = (num1 * num2) / 100
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optExponenciacion.Checked Then
-            respuesta = num1 ^ num2
-            lblRespuesta.Text = respuesta
-        End If
-
-        If optResiduo.Checked Then
-            respuesta = num1 Mod num2
-            lblRespuesta.Text = respuesta
-        End If
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 1
-                respuesta = num1 + num2
-                lblRespuesta.Text = respuesta
-            Case 2
-                respuesta = num1 - num2
-                lblRespuesta.Text = respuesta
-            Case 3
-                respuesta = num1 * num2
-                lblRespuesta.Text = respuesta
-            Case 4
-                respuesta = num1 / num2
-                lblRespuesta.Text = respuesta
-            Case 5
-                respuesta = (num1 * num2) / 100
-                lblRespuesta.Text = respuesta
-            Case 6
-                respuesta = num1 ^ num2
-                lblRespuesta.Text = respuesta
-            Case 7
-                respuesta = num1 Mod num2
-                lblRespuesta.Text = respuesta
-        End Select
+    'llenar el combobox al iniciar el programa, departamentos
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cobdepartamento.Items.AddRange(objdireccion.departamento)
     End Sub
+
+    'llenar el combobox municipio segun el departamento
+    Private Sub cobdepartamento_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cobdepartamento.SelectedIndexChanged
+        cobmunicipio.Items.Clear()
+        cobmunicipio.Text = ""
+        'llenar los municipios segun la posicion del combobox departamento
+        cobmunicipio.Items.AddRange(objdireccion.municipio(cobdepartamento.SelectedIndex))
+
+    End Sub
+
+
+
+
+End Class
+
+
+Class direcciones
+
+    Public departamento() As String = {"Selecione un departamento", "Ahuachapán", "Cabañas", "Chalatenango", "Cuscatlán", "Morazán", "La Libertad", "La Paz", "La Unión", "San Miguel", "San Salvador", "San Vicente", "Santa Ana", "Sonsonate", "Usulután"}
+    Public municipio()() As String = {
+        New String() {"Selecione un municipio"},
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "Guaymango", "Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"},     '>ahuachapan
+        New String() {"Cinquera", "Villa Dolores", "Guacotecti", "Ilobasco", "Jutiapa", "San Isidro", "Sensuntepeque", "Tejutepeque", "Victoria"},                                                             '>Cabañas
+        New String() {"Agua Caliente", "Arcatao", "Azacualpa", "Chalatenango", "Citalá", "Comalapa", "Concepción Quezaltepeque", "Dulce Nombre de María", "El Carrizal", "El Paraíso", "La Laguna", "La Palma", "La Reina", "Las Vueltas", "Nombre de Jesús", "Nueva Concepción", "Nueva Trinidad", "Ojos de Agua", "Potonico", "San Antonio de la Cruz", "San Antonio Los Ranchos", "San Fernando", "San Francisco Lempa", "San Francisco Morazán", "San Ignacio", "San Isidro Labrador", "San José Cancasque", "San José Las Flores", "San Luis del Carmen", "San Miguel de Mercedes", "San Rafael", "Santa Rita", "Tejutla"}, '>Chalatenango
+        New String() {"Candelaria", "Cojutepeque", "El Carmen", "El Rosario", "Monte San Juano", "Oratorio de Concepción", "San Bartolomé Perulapía", "San Cristóbal", "San José Guayabal", "San Pedro Perulapán", "San Rafael Cedros", "San Ramón", "Santa Cruz Analquito", "Santa Cruz Michapa", "Suchitoto", "Tenancingo"}, '>Cuscatlán
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>Morazán
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>La Libertad
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>La Paz
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>La Unión
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>San Miguel
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>San Salvador
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>San Vicente
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>Santa Ana
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}, '>Sonsonate
+        New String() {"Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "	Guaymango", "	Jujutla", "San Francisco Menéndez", "San Lorenzo", "San Pedro Puxtla", "Tacuba", "Turín"}  '>Usulután
+      }
 End Class
