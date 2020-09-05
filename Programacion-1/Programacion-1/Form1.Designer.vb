c367377c4ecedf4bf8698bbaa0e48d88cd2649e4
alexwam032120 <alexwam03@outlook.com>
2020-09-05 16:57:25 -0600

    Recuperacion de codigo

--- Programacion-1/Programacion-1/Form1.Designer.vb
+++ Programacion-1/Programacion-1/Form1.Designer.vb
@@ -22,269 +22,201 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.btnlimpiar = New System.Windows.Forms.Button()
-        Me.btncalcular = New System.Windows.Forms.Button()
-        Me.txtresultado = New System.Windows.Forms.TextBox()
-        Me.txtunidad = New System.Windows.Forms.TextBox()
-        Me.txtcantidad = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblRespuesta = New System.Windows.Forms.Label()
+        Me.optResiduo = New System.Windows.Forms.RadioButton()
+        Me.optPorcentaje = New System.Windows.Forms.RadioButton()
+        Me.optExponenciacion = New System.Windows.Forms.RadioButton()
+        Me.optDivicion = New System.Windows.Forms.RadioButton()
+        Me.optMultiplicacion = New System.Windows.Forms.RadioButton()
+        Me.optResta = New System.Windows.Forms.RadioButton()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtnum2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.btncalcularsuperficie = New System.Windows.Forms.Button()
-        Me.lblresultado = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.coba = New System.Windows.Forms.ComboBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.cobde = New System.Windows.Forms.ComboBox()
-        Me.txtcantidadsuperficie = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.TabControl1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
-        Me.TabPage2.SuspendLayout()
+        Me.optSuma = New System.Windows.Forms.RadioButton()
+        Me.txtnum1 = New System.Windows.Forms.TextBox()
+        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TabControl1
+        'lblRespuesta
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(423, 384)
-        Me.TabControl1.TabIndex = 0
+        Me.lblRespuesta.AutoSize = True
+        Me.lblRespuesta.Location = New System.Drawing.Point(297, 42)
+        Me.lblRespuesta.Name = "lblRespuesta"
+        Me.lblRespuesta.Size = New System.Drawing.Size(0, 13)
+        Me.lblRespuesta.TabIndex = 41
         '
-        'TabPage1
+        'optResiduo
         '
-        Me.TabPage1.Controls.Add(Me.btnlimpiar)
-        Me.TabPage1.Controls.Add(Me.btncalcular)
-        Me.TabPage1.Controls.Add(Me.txtresultado)
-        Me.TabPage1.Controls.Add(Me.txtunidad)
-        Me.TabPage1.Controls.Add(Me.txtcantidad)
-        Me.TabPage1.Controls.Add(Me.Label3)
-        Me.TabPage1.Controls.Add(Me.Label2)
-        Me.TabPage1.Controls.Add(Me.Label1)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(415, 358)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "Mi conversor"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.optResiduo.AutoSize = True
+        Me.optResiduo.Location = New System.Drawing.Point(142, 172)
+        Me.optResiduo.Name = "optResiduo"
+        Me.optResiduo.Size = New System.Drawing.Size(64, 17)
+        Me.optResiduo.TabIndex = 40
+        Me.optResiduo.TabStop = True
+        Me.optResiduo.Text = "Residuo"
+        Me.optResiduo.UseVisualStyleBackColor = True
         '
-        'btnlimpiar
+        'optPorcentaje
         '
-        Me.btnlimpiar.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnlimpiar.Location = New System.Drawing.Point(148, 151)
-        Me.btnlimpiar.Name = "btnlimpiar"
-        Me.btnlimpiar.Size = New System.Drawing.Size(91, 53)
-        Me.btnlimpiar.TabIndex = 19
-        Me.btnlimpiar.Text = "Limpiar"
-        Me.btnlimpiar.UseVisualStyleBackColor = True
+        Me.optPorcentaje.AutoSize = True
+        Me.optPorcentaje.Location = New System.Drawing.Point(142, 126)
+        Me.optPorcentaje.Name = "optPorcentaje"
+        Me.optPorcentaje.Size = New System.Drawing.Size(76, 17)
+        Me.optPorcentaje.TabIndex = 39
+        Me.optPorcentaje.TabStop = True
+        Me.optPorcentaje.Text = "Porcentaje"
+        Me.optPorcentaje.UseVisualStyleBackColor = True
         '
-        'btncalcular
+        'optExponenciacion
         '
-        Me.btncalcular.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btncalcular.Location = New System.Drawing.Point(35, 151)
-        Me.btncalcular.Name = "btncalcular"
-        Me.btncalcular.Size = New System.Drawing.Size(91, 53)
-        Me.btncalcular.TabIndex = 18
-        Me.btncalcular.Text = "calcular"
-        Me.btncalcular.UseVisualStyleBackColor = True
+        Me.optExponenciacion.AutoSize = True
+        Me.optExponenciacion.Location = New System.Drawing.Point(142, 149)
+        Me.optExponenciacion.Name = "optExponenciacion"
+        Me.optExponenciacion.Size = New System.Drawing.Size(31, 17)
+        Me.optExponenciacion.TabIndex = 38
+        Me.optExponenciacion.TabStop = True
+        Me.optExponenciacion.Text = "^"
+        Me.optExponenciacion.UseVisualStyleBackColor = True
         '
-        'txtresultado
+        'optDivicion
         '
-        Me.txtresultado.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtresultado.Location = New System.Drawing.Point(90, 106)
-        Me.txtresultado.Name = "txtresultado"
-        Me.txtresultado.Size = New System.Drawing.Size(127, 26)
-        Me.txtresultado.TabIndex = 17
+        Me.optDivicion.AutoSize = True
+        Me.optDivicion.Location = New System.Drawing.Point(142, 103)
+        Me.optDivicion.Name = "optDivicion"
+        Me.optDivicion.Size = New System.Drawing.Size(30, 17)
+        Me.optDivicion.TabIndex = 37
+        Me.optDivicion.TabStop = True
+        Me.optDivicion.Text = "/"
+        Me.optDivicion.UseVisualStyleBackColor = True
         '
-        'txtunidad
+        'optMultiplicacion
         '
-        Me.txtunidad.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtunidad.Location = New System.Drawing.Point(90, 66)
-        Me.txtunidad.Name = "txtunidad"
-        Me.txtunidad.Size = New System.Drawing.Size(127, 26)
-        Me.txtunidad.TabIndex = 16
+        Me.optMultiplicacion.AutoSize = True
+        Me.optMultiplicacion.Location = New System.Drawing.Point(142, 80)
+        Me.optMultiplicacion.Name = "optMultiplicacion"
+        Me.optMultiplicacion.Size = New System.Drawing.Size(29, 17)
+        Me.optMultiplicacion.TabIndex = 36
+        Me.optMultiplicacion.TabStop = True
+        Me.optMultiplicacion.Text = "*"
+        Me.optMultiplicacion.UseVisualStyleBackColor = True
         '
-        'txtcantidad
+        'optResta
         '
-        Me.txtcantidad.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtcantidad.Location = New System.Drawing.Point(90, 20)
-        Me.txtcantidad.Name = "txtcantidad"
-        Me.txtcantidad.Size = New System.Drawing.Size(127, 26)
-        Me.txtcantidad.TabIndex = 15
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(16, 106)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(73, 20)
-        Me.Label3.TabIndex = 14
-        Me.Label3.Text = "Resultado:"
+        Me.optResta.AutoSize = True
+        Me.optResta.Location = New System.Drawing.Point(142, 57)
+        Me.optResta.Name = "optResta"
+        Me.optResta.Size = New System.Drawing.Size(28, 17)
+        Me.optResta.TabIndex = 35
+        Me.optResta.TabStop = True
+        Me.optResta.Text = "-"
+        Me.optResta.UseVisualStyleBackColor = True
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(19, 66)
+        Me.Label2.Location = New System.Drawing.Point(179, 38)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(55, 20)
-        Me.Label2.TabIndex = 13
-        Me.Label2.Text = "Unidad:"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 34
+        Me.Label2.Text = "Numero 1"
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(238, 35)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(53, 20)
+        Me.txtnum2.TabIndex = 33
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(19, 23)
+        Me.Label1.Location = New System.Drawing.Point(12, 38)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(65, 20)
-        Me.Label1.TabIndex = 12
-        Me.Label1.Text = "Cantidad:"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 32
+        Me.Label1.Text = "Numero 1"
         '
-        'TabPage2
+        'optSuma
         '
-        Me.TabPage2.Controls.Add(Me.btncalcularsuperficie)
-        Me.TabPage2.Controls.Add(Me.lblresultado)
-        Me.TabPage2.Controls.Add(Me.Label7)
-        Me.TabPage2.Controls.Add(Me.Label6)
-        Me.TabPage2.Controls.Add(Me.coba)
-        Me.TabPage2.Controls.Add(Me.Label5)
-        Me.TabPage2.Controls.Add(Me.cobde)
-        Me.TabPage2.Controls.Add(Me.txtcantidadsuperficie)
-        Me.TabPage2.Controls.Add(Me.Label4)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(415, 358)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Area (Superficie)"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        Me.optSuma.AutoSize = True
+        Me.optSuma.Checked = True
+        Me.optSuma.Location = New System.Drawing.Point(142, 34)
+        Me.optSuma.Name = "optSuma"
+        Me.optSuma.Size = New System.Drawing.Size(31, 17)
+        Me.optSuma.TabIndex = 31
+        Me.optSuma.TabStop = True
+        Me.optSuma.Text = "+"
+        Me.optSuma.UseVisualStyleBackColor = True
         '
-        'btncalcularsuperficie
+        'txtnum1
         '
-        Me.btncalcularsuperficie.Location = New System.Drawing.Point(139, 133)
-        Me.btncalcularsuperficie.Name = "btncalcularsuperficie"
-        Me.btncalcularsuperficie.Size = New System.Drawing.Size(112, 56)
-        Me.btncalcularsuperficie.TabIndex = 8
-        Me.btncalcularsuperficie.Text = "Calcular"
-        Me.btncalcularsuperficie.UseVisualStyleBackColor = True
+        Me.txtnum1.Location = New System.Drawing.Point(71, 35)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(53, 20)
+        Me.txtnum1.TabIndex = 30
         '
-        'lblresultado
+        'cboOperaciones
         '
-        Me.lblresultado.AutoSize = True
-        Me.lblresultado.Location = New System.Drawing.Point(167, 216)
-        Me.lblresultado.Name = "lblresultado"
-        Me.lblresultado.Size = New System.Drawing.Size(0, 13)
-        Me.lblresultado.TabIndex = 7
+        Me.cboOperaciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboOperaciones.FormattingEnabled = True
+        Me.cboOperaciones.Items.AddRange(New Object() {"Seleccione una opción", "Suma", "Resta", "Multiplicación", "División", "Porcentaje", "Exponenciación", "Residuo"})
+        Me.cboOperaciones.Location = New System.Drawing.Point(255, 122)
+        Me.cboOperaciones.Name = "cboOperaciones"
+        Me.cboOperaciones.Size = New System.Drawing.Size(121, 21)
+        Me.cboOperaciones.TabIndex = 29
         '
-        'Label7
+        'Button1
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(103, 216)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(58, 13)
-        Me.Label7.TabIndex = 6
-        Me.Label7.Text = "Resultado:"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(206, 87)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(14, 13)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "A"
-        '
-        'coba
-        '
-        Me.coba.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.coba.FormattingEnabled = True
-        Me.coba.Location = New System.Drawing.Point(226, 84)
-        Me.coba.Name = "coba"
-        Me.coba.Size = New System.Drawing.Size(121, 21)
-        Me.coba.TabIndex = 4
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 87)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(64, 13)
-        Me.Label5.TabIndex = 3
-        Me.Label5.Text = "Convertir de"
-        '
-        'cobde
-        '
-        Me.cobde.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cobde.FormattingEnabled = True
-        Me.cobde.Location = New System.Drawing.Point(79, 84)
-        Me.cobde.Name = "cobde"
-        Me.cobde.Size = New System.Drawing.Size(121, 21)
-        Me.cobde.TabIndex = 2
-        '
-        'txtcantidadsuperficie
-        '
-        Me.txtcantidadsuperficie.Location = New System.Drawing.Point(61, 27)
-        Me.txtcantidadsuperficie.Name = "txtcantidadsuperficie"
-        Me.txtcantidadsuperficie.Size = New System.Drawing.Size(100, 20)
-        Me.txtcantidadsuperficie.TabIndex = 1
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(3, 30)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(52, 13)
-        Me.Label4.TabIndex = 0
-        Me.Label4.Text = "Cantidad:"
+        Me.Button1.Location = New System.Drawing.Point(160, 195)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 53)
+        Me.Button1.TabIndex = 28
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(449, 408)
-        Me.Controls.Add(Me.TabControl1)
+        Me.ClientSize = New System.Drawing.Size(411, 290)
+        Me.Controls.Add(Me.lblRespuesta)
+        Me.Controls.Add(Me.optResiduo)
+        Me.Controls.Add(Me.optPorcentaje)
+        Me.Controls.Add(Me.optExponenciacion)
+        Me.Controls.Add(Me.optDivicion)
+        Me.Controls.Add(Me.optMultiplicacion)
+        Me.Controls.Add(Me.optResta)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtnum2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.optSuma)
+        Me.Controls.Add(Me.txtnum1)
+        Me.Controls.Add(Me.cboOperaciones)
+        Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Parcial I"
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
-        Me.TabPage1.PerformLayout()
-        Me.TabPage2.ResumeLayout(False)
-        Me.TabPage2.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents txtresultado As TextBox
-    Friend WithEvents txtunidad As TextBox
-    Friend WithEvents txtcantidad As TextBox
-    Friend WithEvents Label3 As Label
+    Friend WithEvents lblRespuesta As Label
+    Friend WithEvents optResiduo As RadioButton
+    Friend WithEvents optPorcentaje As RadioButton
+    Friend WithEvents optExponenciacion As RadioButton
+    Friend WithEvents optDivicion As RadioButton
+    Friend WithEvents optMultiplicacion As RadioButton
+    Friend WithEvents optResta As RadioButton
     Friend WithEvents Label2 As Label
+    Friend WithEvents txtnum2 As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents btncalcular As Button
-    Friend WithEvents btnlimpiar As Button
-    Friend WithEvents lblresultado As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents coba As ComboBox
-    Friend WithEvents Label5 As Label
-    Friend WithEvents cobde As ComboBox
-    Friend WithEvents txtcantidadsuperficie As TextBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents btncalcularsuperficie As Button
+    Friend WithEvents optSuma As RadioButton
+    Friend WithEvents txtnum1 As TextBox
+    Friend WithEvents cboOperaciones As ComboBox
+    Friend WithEvents Button1 As Button
 End Class
--- Programacion-1/Programacion-1/Form1.vb
+++ Programacion-1/Programacion-1/Form1.vb
@@ -1,105 +1,73 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-    'clases
-    Dim objcalculos = New calculos
-    Dim objconvertidor = New conversor
+        Dim num1, num2, respuesta As Double
 
 
-    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
-        Dim serie() = txtresultado.Text.Split("/")
-
+        num1 = txtnum1.Text
+        num2 = txtnum2.Text
 
 
 
-        'si al menos un campo esta con datos
-        If txtcantidad.Text <> "" Or txtresultado.Text <> "" Or txtunidad.Text <> "" Then
-            'si cantidad esta vacia, y hay unidades y grupos, calcular la cantidad
-            If txtcantidad.Text = "" And txtunidad.Text <> "" And txtresultado.Text <> "" Then
-                'enviar datos a la clase llamada mi conversor con la variable enviounidad
-                objcalculos.enviounidad = txtunidad.Text
-                'enviar arreglo dividido con / y hacer llamado de la funcion rcantidad
-                txtcantidad.Text = objcalculos.rcantidad(serie)
-
-                'si solo tiene datos el campo de resultado
-            ElseIf txtresultado.Text <> "" And txtcantidad.Text = "" And txtunidad.Text = "" Then
-                Dim j As Integer
-                For Each ewe In serie
-                    j = j + ewe
-                Next
-                txtcantidad.Text = j
-                txtunidad.Text = 1
-
-                'si unidades esta vacio y resultado esta vacio
-            ElseIf txtunidad.Text = "" And txtcantidad.Text <> "" Then
-                'resultados tomara el valor de cantidad, y 0 unidades
-                txtresultado.Text = (txtcantidad.Text).ToString + "/" + (0).ToString
-
-                'si resultado esta vacio y cantidad y unidades tiene datos
-            ElseIf txtresultado.Text = "" And txtcantidad.Text <> "" And txtunidad.Text <> "" Then
-                'resultado tomara el valor correspondiente
-                txtresultado.Text = (txtcantidad.Text \ txtunidad.Text).ToString + "/" + (txtcantidad.Text Mod txtunidad.Text).ToString
-
-
-            End If
-            'si todos los campos estan vacios, enviar un mensaje
-        ElseIf txtcantidad.Text = "" And txtresultado.Text = "" And txtunidad.Text = "" Then
-            MessageBox.Show("Por favor rellena algunos datos")
+        If optSuma.Checked Then
+            respuesta = num1 + num2
+            lblRespuesta.Text = respuesta
         End If
-    End Sub
 
-    'limpiar campos
-    Private Sub btnlimpiar_Click(sender As Object, e As EventArgs) Handles btnlimpiar.Click
-        txtcantidad.Clear()
-        txtresultado.Clear()
-        txtunidad.Clear()
-    End Sub
+        If optResta.Checked Then
+            respuesta = num1 - num2
+            lblRespuesta.Text = respuesta
+        End If
 
+        If optMultiplicacion.Checked Then
+            respuesta = num1 * num2
+            lblRespuesta.Text = respuesta
+        End If
 
+        If optDivicion.Checked Then
+            respuesta = num1 / num2
+            lblRespuesta.Text = respuesta
+        End If
 
+        If optPorcentaje.Checked Then
+            respuesta = (num1 * num2) / 100
+            lblRespuesta.Text = respuesta
+        End If
 
-    '///////////////////////////////////////////////////////////////////////////////////////////////////////////
+        If optExponenciacion.Checked Then
+            respuesta = num1 ^ num2
+            lblRespuesta.Text = respuesta
+        End If
 
-
-
-    'cargar datos a combobox de conversor de area
-    Private Sub cobde_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cobde.SelectedIndexChanged
-        coba.Items.Clear()
-        coba.Items.AddRange(objconvertidor.convertira)
-    End Sub
-
-    'cargar datos a combobox de conversor de area
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cobde.Items.Clear()
-        cobde.Items.AddRange(objconvertidor.convertirde)
-    End Sub
-
-
-    'calcular conversion de area, los datos son enviados a la clase convertidor
-    Private Sub btncalcularsuperficie_Click(sender As Object, e As EventArgs) Handles btncalcularsuperficie.Click
-
-        If txtcantidadsuperficie.Text <> "" And cobde.SelectedIndex >= 0 And coba.SelectedIndex >= 0 Then
-            Dim posicioncobde, posicioncoba As SByte
-            'toma la posicion selecionada
-            posicioncobde = cobde.SelectedIndex
-            posicioncoba = coba.SelectedIndex
-
-            'enviar datos a la clase
-            objconvertidor.enviocantidadacalcular = txtcantidadsuperficie.Text
-            objconvertidor.envioposicioncobde = posicioncobde
-            objconvertidor.envioposicioncoba = posicioncoba
-            'hace llamado a la funcion calculo area dentro de la calse convertidor
-            lblresultado.Text = objconvertidor.calculodearea
-
-        ElseIf txtcantidadsuperficie.Text = "" Or cobde.SelectedIndex < 0 Or coba.SelectedIndex < 0 Then
-            MessageBox.Show("Por favor rellena todos los datos")
+        If optResiduo.Checked Then
+            respuesta = num1 Mod num2
+            lblRespuesta.Text = respuesta
         End If
 
 
 
-
+        Select Case cboOperaciones.SelectedIndex
+            Case 1
+                respuesta = num1 + num2
+                lblRespuesta.Text = respuesta
+            Case 2
+                respuesta = num1 - num2
+                lblRespuesta.Text = respuesta
+            Case 3
+                respuesta = num1 * num2
+                lblRespuesta.Text = respuesta
+            Case 4
+                respuesta = num1 / num2
+                lblRespuesta.Text = respuesta
+            Case 5
+                respuesta = (num1 * num2) / 100
+                lblRespuesta.Text = respuesta
+            Case 6
+                respuesta = num1 ^ num2
+                lblRespuesta.Text = respuesta
+            Case 7
+                respuesta = num1 Mod num2
+                lblRespuesta.Text = respuesta
+        End Select
     End Sub
-
 End Class
-
-
-
